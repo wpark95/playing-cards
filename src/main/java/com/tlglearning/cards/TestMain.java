762ddca06cdf36b25cd0f098a31f72df99bbc595
wpark95 <park.wongyun@gmail.com>
2022-07-29 13:39:48 -0700

    Moved rank-first comparator inside TestMain.

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -2,8 +2,8 @@ package com.tlglearning.cards;
 
 import com.tlglearning.cards.model.Card;
 import com.tlglearning.cards.model.Deck;
-import com.tlglearning.cards.strategy.RankFirstComparator;
 import java.security.SecureRandom;
+import java.util.Comparator;
 import java.util.Random;
 
 public class TestMain {
@@ -25,4 +25,16 @@ public class TestMain {
     System.out.println(deck);
   }
 
+  static class RankFirstComparator implements Comparator<Card> {
+
+    @Override
+    public int compare(Card card1, Card card2) {
+      int comparison = card1.getRank().compareTo(card2.getRank());
+      if (comparison == 0) {
+        comparison = card1.getSuit().compareTo(card2.getSuit());
+      }
+      return comparison;
+    }
+
+  }
 }
